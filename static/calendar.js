4926b5e22360c1d955b541cb1941d2607e92a051
Stupnikjs <106406099+Stupnikjs@users.noreply.github.com>
2024-07-14 10:20:26 +0200

    Update calendar.js

--- static/calendar.js
+++ static/calendar.js
@@ -1,3 +1,8 @@
 let currMonth = new Date()
 // increment month button 
 
+const  Months = ["Janvier", "Fevrier", "Mars", "Avril", "Mai", "Juin", "Juillet", "Aout", "Septembre","Octobre", "Novembre", "Decembre"]
+
+
+const  daysOfWeek = ["L", "M", "M","J", "V", "S", "D"] 
+
