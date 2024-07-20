2daef9e7051db996dc75937de5d43e8533936c0a
Stupnikjs <106406099+Stupnikjs@users.noreply.github.com>
2024-07-20 22:31:35 +0200

    Update repository.go

--- repo/repository.go
+++ repo/repository.go
@@ -6,6 +6,9 @@ type Rempla struct {
 	Fin          string `json:"fin"`
 	Lieu         string `json:"lieu"`
 	Logiciel     string `json:"logiciel"`
+    Created_at string 
+    Updated_at string 
+    Validated bool `json::"validated"`
 	Retrocession int    `json:"retrocession"`
 	Temps_trajet int    `json:"temps_trajet"`
 }
