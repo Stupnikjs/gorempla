79c2aaf91cc4bff4f0d958ffef53d3559b64ba31
Stupnikjs <106406099+Stupnikjs@users.noreply.github.com>
2024-07-28 13:16:35 +0200

    Update monthincrement.js

--- static/js/monthincrement.js
+++ static/js/monthincrement.js
@@ -1,1 +1,20 @@
-.
+let today = new Date()
+createCalendar(mocksRemplas, today)
+
+let plusBtn = document.querySelector("#plusBtn")
+let currMonthSpan = document querySelector("#currMonth")
+let currMonth = Months[today.getMonth()]
+currMonthSpan.textContent = currMonth
+plusBtn.addEventListener("click", (e) => {
+    let old = document.querySelector("#calendarDiv")
+    old.remove()
+    if (today.getMonth() != 11){
+    today = new Date(today.getFullYear(), today.getMonth() + 1, today.getDate())
+
+    createCalendar(mocksRemplas, today)
+    } else {
+        today = new Date(today.getFullYear() + 1, 0, today.getDate())
+        createCalendar(mocksRemplas, today)
+    }
+
+})
