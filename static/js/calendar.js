4acdca640cfad07296a8de2b730d50231071b32d
Stupnikjs <106406099+Stupnikjs@users.noreply.github.com>
2024-07-28 13:17:13 +0200

    Update calendar.js

--- static/js/calendar.js
+++ static/js/calendar.js
@@ -282,30 +282,6 @@ function coordinateFromBoolArr(boolArr){
 
 
 
-/* 
-* onclick + increment today and pass  *createCalendar
-*/
-
-let today = new Date()
-createCalendar(mocksRemplas, today)
-
-let plusBtn = document.querySelector("#plusBtn")
-let currMonthSpan = document querySelector("#currMonth")
-let currMonth = Months[today.getMonth()]
-currMonthSpan.textContent = currMonth
-plusBtn.addEventListener("click", (e) => {
-    let old = document.querySelector("#calendarDiv")
-    old.remove()
-    if (today.getMonth() != 11){
-    today = new Date(today.getFullYear(), today.getMonth() + 1, today.getDate())
-    
-    createCalendar(mocksRemplas, today)
-    } else {
-        today = new Date(today.getFullYear() + 1, 0, today.getDate())
-        createCalendar(mocksRemplas, today)
-    }
-    
-    })
 
 
 
